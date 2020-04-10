3dc4d5b432fa7df50af2b670b4b1b02827be9dc6
Blake Myers <blaketoyo@live.com>
2020-04-09 19:26:00 -0700

    First attempt at Editor script and PGC terrain script working together

---
+++ Assets/Editor/PGCTerrainEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PGCTerrain))]
+public class PGCTerrainEditor : Editor
+{
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+        PGCTerrain PGCTerrain = (PGCTerrain)target;
+
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Random Terrain"))
+        {
+            PGCTerrain.ResizeTerrain();
+            Debug.Log("Random terrain");
+        }
+        if (GUILayout.Button("Single Perlin"))
+            Debug.Log("Single Perlin");
+        GUILayout.EndHorizontal();
+    }
+}
---
+++ Assets/Scripts/PGCTerrain.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PGCTerrain : MonoBehaviour
+{
+    public int mapWidth = 256;
+    public int mapHeight = 256;
+    public int mapDepth = 20;
+
+    private void start()
+    {
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData = GenerateTerrain(terrain.terrainData);
+    }
+    TerrainData GenerateTerrain(TerrainData terrainData) {
+        terrainData.size = new Vector3(mapWidth, mapDepth, mapHeight);
+        return terrainData;
+    }
+
+    public void ResizeTerrain() { 
+    Terrain terrain2 = GetComponent<Terrain>();
+    terrain2.terrainData = GenerateTerrain(terrain2.terrainData);
+    }
+}
