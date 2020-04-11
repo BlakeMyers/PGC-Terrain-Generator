0de6499a63706e2438b897d1c34be5c93fb99b3e
Blake Myers <blaketoyo@live.com>
2020-04-11 09:08:07 -0700

    Made Map size more User friendly

--- Assets/Editor/PGCTerrainEditor.cs
+++ Assets/Editor/PGCTerrainEditor.cs
@@ -6,12 +6,28 @@ public class PGCTerrainEditor : Editor
 {
     bool showPer = false;
     bool showMPO = false;
+    int mapsize;
     public override void OnInspectorGUI() {
+
         PGCTerrain PGCTerrain = (PGCTerrain)target;
+
         EditorGUILayout.LabelField("Map Options");
-        PGCTerrain.mapWidth = EditorGUILayout.IntField("Map Width",PGCTerrain.mapWidth);
-        PGCTerrain.mapHeight = EditorGUILayout.IntField("Map Height", PGCTerrain.mapHeight);
+
+        mapsize = EditorGUILayout.IntField("Map Width (2^n)", mapsize);
+
+        if (Mathf.Floor(Mathf.Log(mapsize, 2)) == Mathf.Ceil(Mathf.Log(mapsize, 2)))
+        {
+            PGCTerrain.mapHeight = mapsize;
+            PGCTerrain.mapWidth = mapsize;
+        }
+        else {
+            mapsize = 512;
+            PGCTerrain.mapHeight = mapsize;
+            PGCTerrain.mapWidth = mapsize;
+        }
+
         PGCTerrain.mapDepth = EditorGUILayout.IntField("Map Depth", PGCTerrain.mapDepth);
+
         if (GUILayout.Button("Random Terrain"))
         {
             PGCTerrain.RandomTerrain();
@@ -19,16 +35,20 @@ public class PGCTerrainEditor : Editor
         }
 
         showPer = EditorGUILayout.Foldout(showPer, "Perlin Noise Options");
+
         if (showPer)
         {
             PGCTerrain.scale = EditorGUILayout.Slider("Scale", PGCTerrain.scale, 0.001f, 500);
             PGCTerrain.Offsets = EditorGUILayout.Vector2Field("Offsets", PGCTerrain.Offsets);
+
             if (GUILayout.Button("Single Perlin"))
             {
                 Debug.Log("Single Perlin");
                 PGCTerrain.SinglePerlin();
             }
+
             showMPO = EditorGUILayout.Foldout(showMPO, "Multiple Perlin Options");
+
             if (showMPO)
             {
                 PGCTerrain.seed = EditorGUILayout.IntField("Map Seed", PGCTerrain.seed);
